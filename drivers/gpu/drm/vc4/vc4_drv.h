69476708546684645acc6a2b4107ca6b7af206ac
Maxime Ripard <maxime@cerno.tech>
2023-02-17 15:33:23 +0100

    drm/vc4: crtc: Add support for BCM2712 PixelValves
    The PixelValves found on the BCM2712 are similar to the ones found in
    the previous generation.
    
    Compared to BCM2711, the pixelvalves only drive one HDMI controller each
    and HDMI1 PixelValve has a FIFO long enough to support 4k at 60Hz.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_drv.h
+++ drivers/gpu/drm/vc4/vc4_drv.h
@@ -549,16 +549,16 @@ struct vc4_pv_data {
 	enum vc4_encoder_type encoder_types[4];
 };
 
-struct vc5_gamma_entry {
-	u32 x_c_terms;
-	u32 grad_term;
-};
-
-#define VC5_HVS_SET_GAMMA_ENTRY(x, c, g) (struct vc5_gamma_entry){	\
-	.x_c_terms = VC4_SET_FIELD((x), SCALER5_DSPGAMMA_OFF_X) | 	\
-		     VC4_SET_FIELD((c), SCALER5_DSPGAMMA_OFF_C),	\
-	.grad_term = (g)						\
-}
+extern const struct vc4_pv_data bcm2835_pv0_data;
+extern const struct vc4_pv_data bcm2835_pv1_data;
+extern const struct vc4_pv_data bcm2835_pv2_data;
+extern const struct vc4_pv_data bcm2711_pv0_data;
+extern const struct vc4_pv_data bcm2711_pv1_data;
+extern const struct vc4_pv_data bcm2711_pv2_data;
+extern const struct vc4_pv_data bcm2711_pv3_data;
+extern const struct vc4_pv_data bcm2711_pv4_data;
+extern const struct vc4_pv_data bcm2712_pv0_data;
+extern const struct vc4_pv_data bcm2712_pv1_data;
 
 struct vc4_crtc {
 	struct drm_crtc base;
