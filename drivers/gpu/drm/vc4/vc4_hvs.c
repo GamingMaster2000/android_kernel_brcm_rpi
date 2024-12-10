4b0aa94c2d9264c8e1cfa52d40de8357df92812d
Dom Cobley <popcornmix@gmail.com>
2024-12-10 15:16:18 +0000

    drm/vc4: hvs: Partial revert of Support BCM2712 HVS
    This partially reverts 14731498fedbf22927cac3969c0c99349d5b8ee9
    to avoid an upcoming merge conflict
    
    Signed-off-by: Dom Cobley <popcornmix@gmail.com>

--- drivers/gpu/drm/vc4/vc4_hvs.c
+++ drivers/gpu/drm/vc4/vc4_hvs.c
@@ -1539,7 +1539,18 @@ struct vc4_hvs *__vc4_hvs_alloc(struct vc4_dev *vc4,
 		return ERR_PTR(-ENODEV);
 	}
 
+<<<<<<< HEAD
 	drm_mm_init(&hvs->dlist_mm, dlist_start, dlist_size);
+=======
+	/* Set up the HVS display list memory manager.  We never
+	 * overwrite the setup from the bootloader (just 128b out of
+	 * our 16K), since we don't want to scramble the screen when
+	 * transitioning from the firmware's boot setup to runtime.
+	 */
+	drm_mm_init(&hvs->dlist_mm,
+		    HVS_BOOTLOADER_DLIST_END,
+		    (SCALER_DLIST_SIZE >> 2) - HVS_BOOTLOADER_DLIST_END);
+>>>>>>> drm/vc4: hvs: Partial revert of Support BCM2712 HVS
 
 	/* Set up the HVS LBM memory manager.  We could have some more
 	 * complicated data structure that allowed reuse of LBM areas
@@ -1993,8 +2004,6 @@ static int vc4_hvs_bind(struct device *dev, struct device *master, void *data)
 	else
 		hvs->dlist = hvs->regs + SCALER_DLIST_START;
 
-<<<<<<< HEAD
-=======
 	if (vc4->gen >= VC4_GEN_6)
 		ret = vc6_hvs_hw_init(hvs);
 	else
@@ -2002,7 +2011,6 @@ static int vc4_hvs_bind(struct device *dev, struct device *master, void *data)
 	if (ret)
 		return ret;
 
->>>>>>> drm/vc4: hvs: Support BCM2712 HVS
 	/* Upload filter kernels.  We only have the one for now, so we
 	 * keep it around for the lifetime of the driver.
 	 */
