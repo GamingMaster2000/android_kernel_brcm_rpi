69777b1a70bee4e0f4fa460e09b59334cc90a2cf
Kelvin <kelvin@nas.gm2k.net>
2025-03-27 20:58:58 +1100

    Test

--- drivers/gpu/drm/vc4/vc4_drv.h
+++ drivers/gpu/drm/vc4/vc4_drv.h
@@ -397,7 +397,7 @@ struct vc4_hvs {
 };*/
 
 #define to_vc4_hvs_state(_state)				\
-	container_of_const(_state, struct vc4_hvs_state, base)
+	container_of(_state, struct vc4_hvs_state, base)
 
 struct vc4_hvs_state *vc4_hvs_get_global_state(struct drm_atomic_state *state);
 // struct vc4_hvs_state *vc4_hvs_get_old_global_state(const struct drm_atomic_state *state);
